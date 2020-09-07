07118be6d92fbc6a9ec3b1cf7fadbb59ea31060d
deanssmart <deanssmart@gmail.com>
2020-09-07 15:19:00 +0100

    Player 1 and 2 increment buttons working

--- src/index.js
+++ src/index.js
@@ -4,12 +4,58 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import { createStore } from 'redux';
+
+const initial = {
+  player1: 0,
+  player2: 0,
+};
+
+const incrementP1 = (state) => {
+  return { ...state, player1: state.player1 + 1 };
+}
+
+const incrementP2 = (state) => {
+  return { ...state, player2: state.player2 + 1 };
+}
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "INCREMENT_P1": return incrementP1(state);
+    case "INCREMENT_P2": return incrementP2(state);
+    default: return state;
+  }
+};
+
+const store = createStore(
+  reducer, 
+  initial,
+  window.__REDUX_DEVTOOLS_EXTENSION__
+  && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
+
+
+const render = () => {
+  let state = store.getState();
+
+  ReactDOM.render(
+    <React.StrictMode>
+      <App
+        scoreP1={ state.player1 }
+        handleIncrementP1={ () => store.dispatch({ type: "INCREMENT_P1" })}
+        scoreP2={ state.player2 }
+        handleIncrementP2={ () => store.dispatch({ type: "INCREMENT_P2" })}
+      />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+  
+}
+
+store.subscribe(render);
+render();
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
