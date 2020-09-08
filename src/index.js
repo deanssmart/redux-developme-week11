24a531611d06222413c95953caa2b518e0eaee4b
deanssmart <deanssmart@gmail.com>
2020-09-08 12:43:23 +0100

    winner and server functions refactored

--- src/index.js
+++ src/index.js
@@ -13,10 +13,16 @@ const initial = {
   winner: 0,
 };
 
+const alternate = (state) => {
+  return (state.player1 >= 20 && state.player2 >= 20) ? 2 : 5;  
+}
+
 const server = (state) => {
+  const total = state.player1 + state.player2;
+
   return { 
     ...state, 
-    player1Serving: (state.player1 >= 20 && state.player2 >= 20) ? !state.player1Serving : (state.player1 + state.player2) % 5 === 0 ? !state.player1Serving : state.player1Serving
+    player1Serving: total % alternate(state) === 0 ? !state.player1Serving : state.player1Serving
   }
 };
 
@@ -27,11 +33,18 @@ const score = (state, { player }) => {
   }
 };
 
+const player1Wins = (state) => {
+  return state.player1 >= 21 && state.player1 - state.player2 >= 2;
+};
+
+const player2Wins = (state) => {
+  return state.player2 >= 21 && state.player2 - state.player1 >= 2;
+};
+
 const winner = (state) => {
   return {
     ...state,
-    winner: 
-      state.player1 >= 21 && state.player1 - state.player2 >= 2 ? 1 : (state.player2 >= 21 && state.player2 - state.player1 >= 2 ? 2 : 0)    
+    winner: player1Wins(state) ? 1 : (player2Wins(state) ? 2 : 0)    
   }
 };
 
